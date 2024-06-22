782ead3a3e302c1d85ef20ba61a3a9764b309d2d
Blank <65240255+Kiroharth@users.noreply.github.com>
2024-06-22 02:16:01 +0200

    Retro test website

---
+++ JS/main.js
@@ -0,0 +1,16 @@
+
+function openTwitch() {
+    window.open('https://www.twitch.tv/anny', '_blank',);
+}
+
+function openYouTube() {
+    window.open('https://www.youtube.com/channel/UCDXFo9upBYHPfreF9IGK25g', '_blank',);
+}
+
+function openTwitter() {
+    window.open('https://twitter.com/annytf', '_blank',);
+}
+
+function closeWindow() {
+    console.log("Window closed!");
+}
