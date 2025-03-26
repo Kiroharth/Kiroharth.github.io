8bb82d24ca5a1a6bb5e04a16f38501b056013081
Blank <65240255+Kiroharth@users.noreply.github.com>
2025-03-26 15:08:32 +0100

    button link test phase

---
+++ script.js
@@ -0,0 +1,5 @@
+const myButton = document.getElementById("Projects");
+myButton.addEventListener("click", function() {
+    console.log("Button clicked!");
+    window.location.href = "projects.html";
+  });
